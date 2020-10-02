8a09328d05543e795cd98767122614d5908c3017
qqai <1941397037@qq.com>
2020-10-03 00:38:15 +0800

    对于java基础的复习总结

---
+++ src/qqai/utils/DateFormart.java
@@ -0,0 +1,19 @@
+package qqai.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * @author qqai
+ * @createTime 2020/10/2 23:43
+ * @description：
+ */
+
+public class DateFormart {
+    public static void main(String[] args) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String s = format.format(new Date());
+        System.out.println(s);
+    }
+}
