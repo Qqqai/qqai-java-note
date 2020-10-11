71bbba7d11795e4bab4a31cca476fbac84ba48a1
qqai <1941397037@qq.com>
2020-10-11 08:59:56 +0800

    面试和springmvc多视图配置

---
+++ src/qqai/mianshi/JianXinJinRongKeJi.java
@@ -0,0 +1,35 @@
+package qqai.mianshi;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author qqai
+ * @createTime 2020/10/10 18:57
+ * @description：建信金融
+ */
+
+public class JianXinJinRongKeJi {
+    public static void main(String[] args) {
+        int[] ints = {54,54532,4324,32432};
+        int num = 43243;
+        System.out.println(carsTrans(ints, num));
+    }
+
+    public static int carsTrans(int[] cars, int num) {
+        Arrays.sort(cars);
+        System.out.println(Arrays.toString(cars));
+        int count = 0;
+        if (cars.length == 0 || num == 0 || cars[cars.length - 1] == 0) {
+            return -1;
+        }
+        while (true) {
+            if (num <= 0) {
+                return count;
+            } else {
+                num -= cars[cars.length - 1];
+                count++;
+            }
+        }
+    }
+}
---
+++ src/qqai/shujujiegou/二叉树/GeiDingShunXunHeBianLiFangShiQiuMouGeShunXunBianLiJieGuo.java
@@ -0,0 +1,13 @@
+package qqai.shujujiegou.二叉树;
+
+/**
+ * @author qqai
+ * @createTime 2020/10/10 18:50
+ * @description：给定一个数组和两种遍历方式，求另一个遍历结果
+ */
+
+public class GeiDingShunXunHeBianLiFangShiQiuMouGeShunXunBianLiJieGuo {
+    public static void main(String[] args) {
+
+    }
+}
