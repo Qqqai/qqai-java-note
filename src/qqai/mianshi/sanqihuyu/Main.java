e290e2ac406674c94669d10079757bce83b34386
qqai <1941397037@qq.com>
2020-10-30 14:22:22 +0800

    三七互娱笔试题和一个力扣题

---
+++ src/qqai/mianshi/sanqihuyu/Main.java
@@ -0,0 +1,28 @@
+package qqai.mianshi.sanqihuyu;
+
+/**
+ * @author qqai
+ * @createTime 2020/10/29 18:21
+ * @description：
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        int level = 5;
+        System.out.println(getNum(level));
+    }
+
+    private static int getNum(int level) {
+        int count = 1;
+        while (level != 1) {
+            if (count % 2 == 0) {
+                count = count / 2 * 5;
+            } else {
+                count--;
+                count = count / 2 * 5 + 3;
+            }
+            level--;
+        }
+        return count;
+    }
+}
