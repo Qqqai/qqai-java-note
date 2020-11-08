c2779706325cab45f6a81366174c54f131e35ec6
qqai <1941397037@qq.com>
2020-11-08 14:59:23 +0800

    Volatile

--- src/qqai/volatiletest/TestVolatile.java
+++ src/qqai/volatiletest/TestVolatile.java
@@ -6,7 +6,7 @@ import java.util.Hashtable;
 /**
  * @author qqai
  * @createTime 2020/11/7 16:39
- * @description：volatile
+ * @description：volatile 标记 关注Volatile指令重排序.md
  */
 
 //TODO  没弄
@@ -18,3 +18,51 @@ public class TestVolatile {
         System.out.println(counter);
     }
 }
+
+/*笔记 字节码：
+    {
+  public static volatile int counter;
+    descriptor: I
+    flags: ACC_PUBLIC, ACC_STATIC, ACC_VOLATILE  笔记 标志位
+
+  public qqai.volatiletest.TestVolatile();
+    descriptor: ()V
+    flags: ACC_PUBLIC
+    Code:
+      stack=1, locals=1, args_size=1
+         0: aload_0
+         1: invokespecial #1                  // Method java/lang/Object."<init>":()V
+         4: return
+      LineNumberTable:
+        line 13: 0
+
+  public static void main(java.lang.String[]);
+    descriptor: ([Ljava/lang/String;)V
+    flags: ACC_PUBLIC, ACC_STATIC
+    Code:
+      stack=2, locals=1, args_size=1
+         0: iconst_2
+         1: putstatic     #2                  // Field counter:I  笔记 静态复制
+         4: getstatic     #3                  // Field java/lang/System.out:Ljava/io/PrintStream;
+         7: getstatic     #2                  // Field counter:I  笔记 静态取值
+        10: invokevirtual #4                  // Method java/io/PrintStream.println:(I)V
+        13: return
+      LineNumberTable:
+        line 17: 0
+        line 18: 4
+        line 19: 13
+
+  static {};
+    descriptor: ()V
+    flags: ACC_STATIC
+    Code:
+      stack=1, locals=0, args_size=0
+         0: iconst_1
+         1: putstatic     #2                  // Field counter:I
+         4: return
+      LineNumberTable:
+        line 14: 0
+}
+SourceFile: "TestVolatile.java"
+
+*/
