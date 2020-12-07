652905420a3dc8e4cd3f63dd0265216d41ed4a2c
qqai <1941397037@qq.com>
2020-12-07 17:09:37 +0800

    算法基础

---
+++ src/qqai/suanfa/jinjie/Manacher.java
@@ -0,0 +1,16 @@
+package qqai.suanfa.jinjie;
+
+/**
+ * @author qqai
+ * @createTime 2020/12/6 21:28
+ * @description：最长的回文子串
+ */
+
+public class Manacher {
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/qqai/test/Test05.java
@@ -0,0 +1,17 @@
+package qqai.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author qqai
+ * @createTime 2020/12/6 18:20
+ * @description：
+ */
+
+public class Test05 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(111);
+    }
+}
